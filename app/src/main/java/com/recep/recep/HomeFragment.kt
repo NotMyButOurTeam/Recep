b9a3c354b873f4a070591967d060865b8be5ef33
DzKInShO2 <dzkinsho2@protonmail.com>
2025-06-17 18:41:13 +0800

    tambah tombol refresh untuk Home

--- app/src/main/java/com/recep/recep/HomeFragment.kt
+++ app/src/main/java/com/recep/recep/HomeFragment.kt
@@ -2,12 +2,16 @@ package com.recep.recep
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EdgeEffect
+import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.search.SearchBar
 import com.recep.recep.database.Database
 import com.recep.recep.recycler.RecipeViewAdapter
@@ -27,9 +31,24 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val scroll = view.findViewById<NestedScrollView>(R.id.homeScroll)
         val searchBar = view.findViewById<SearchBar>(R.id.homeSearchBar)
         val recipeList = view.findViewById<RecyclerView>(R.id.homeRecipeList)
+        val refreshButton = view.findViewById<MaterialButton>(R.id.homeRefresh)
 
+        loadContentToRecycler(recipeList)
+        searchBar.setOnClickListener {
+            val searchIntent = Intent(context, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        refreshButton.setOnClickListener {
+            loadContentToRecycler(recipeList)
+            scroll.smoothScrollTo(0, view.top)
+        }
+    }
+
+    private fun loadContentToRecycler(recycler: RecyclerView) {
         if (isAdded) {
             val context = requireContext()
             Database.getRecipes(context, 8) {  list ->
@@ -37,13 +56,8 @@ class HomeFragment : Fragment() {
                 val imageWidthDp = 368
                 val columnCount = maxOf(1, screenWidthDp / imageWidthDp)
 
-                recipeList.layoutManager = GridLayoutManager(context, columnCount)
-                recipeList.adapter = RecipeViewAdapter(list)
-            }
-
-            searchBar.setOnClickListener {
-                val searchIntent = Intent(context, SearchActivity::class.java)
-                startActivity(searchIntent)
+                recycler.layoutManager = GridLayoutManager(context, columnCount)
+                recycler.adapter = RecipeViewAdapter(list)
             }
         }
     }
